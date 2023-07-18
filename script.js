8041ccaa4bd85f75064e58a9b9e17e8571a4925a
Ryan <ryanstanford92@gmail.com>
2023-07-17 20:00:37 -0500

    add function chooseRoundWinner
    function implements game logic to determine winner and assign points

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let choices = ['Rock', 'Paper', 'Scissors'];
+let userPoints = 0;
+let computerPoints = 0;
 
 //gets a random number between 0 and 2 and returns the value at the corresponding index location in the choices array 
 function getComputerChoice() {
@@ -10,4 +12,23 @@ function getComputerChoice() {
 //input a value between 0 and 2 that maps to and returns the corresponding value in the choices array
 function getUserChoice(choice) {
     return choices[choice];
+}
+
+//game logic: rock > scissors, paper > rock, scissors > paper
+//choose who wins the round based on input
+function chooseRoundWinner(userChoice, computerChoice) {
+    if(userChoice == computerChoice) {
+        console.log('TIE');
+        return null;
+    }
+    else if(userChoice == 'Rock' && computerChoice == 'Scissors' ||
+            userChoice == 'Paper' && computerChoice == 'Rock' ||
+            userChoice == 'Scissors' && computerChoice == 'Paper') {
+        console.log('You won the round');
+        return userPoints += 1;
+    }
+    else {
+        console.log('You lost the round');
+        return computerPoints += 1;
+    }
 }
